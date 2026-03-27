4ec222d73b54280dc17df136398bad85cd426946
Abdullahkamanger <abdullahkamanger300@gmail.com>
2026-03-28 03:19:13 +0500

    Update db.js correct the variables to match env

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -5,15 +5,13 @@ dotenv.config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0,
-  ssl: {
-    rejectUnauthorized: true, // This enforces the secure connection TiDB wants
-  }
+  ssl: { rejectUnauthorized: true }
 });
 
 export default pool.promise();
